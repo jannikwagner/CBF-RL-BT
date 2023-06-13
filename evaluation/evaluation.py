6adee1b4b1775ef62a682de250b6c7784b33098e
Jannik Wagner <jannik.wagner@proton.me>
2023-06-13 19:51:00 +0200

    construct augmented episodes array

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -16,3 +16,25 @@ df = pd.DataFrame(data)
 
 steps = df.steps
 global_success = df.globalSuccess
+
+# could instead also be done in unity
+augmented_episodes = []
+
+for i in range(len(data)):
+    btepisode = data[i]
+    actionStatistics = btepisode["actionStatistics"]
+    localEpisodeNumber = 0
+    for action in actionStatistics:
+        actionStatistic = actionStatistics[action]
+        episodes = actionStatistic["episodes"]
+        for j in range(len(episodes)):
+            episode = episodes[j]
+            episode["action"] = action
+            episode["compositeEpisodeNumber"] = i
+            episode["localEpisodeNumber"] = localEpisodeNumber
+            localEpisodeNumber += 1
+            augmented_episodes.append(episode)
+
+df = pd.DataFrame(augmented_episodes)
+
+print(df)
