4a9bd2e523f544f09da40fd425dd48e0ad8373a8
Jannik Wagner <jannik.wagner@proton.me>
2023-06-19 10:13:08 +0200

    add plots

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -5,9 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-runId = "testRunId"
+run_id = "testRunId"
 
-filePath = f"evaluation/stats/{runId}/statistics.json"
+file_path = f"evaluation/stats/{run_id}/statisticsWOCBF.json"
 
 action_termination_cause = [
     "PostConditionReached",
@@ -26,8 +26,8 @@ class Statistics:
     std_global_steps: float
 
 
-def load_repr1_to_eps(filePath):
-    with open(filePath, "r") as f:
+def load_repr1_to_eps(file_path):
+    with open(file_path, "r") as f:
         data = json.load(f)
     # print(data)
 
@@ -69,7 +69,7 @@ def load_repr1_to_eps(filePath):
     return eps_df
 
 
-eps_df = load_repr1_to_eps(filePath)
+eps_df = load_repr1_to_eps(file_path)
 
 actions = eps_df.action.unique()
 accs = eps_df.query("terminationCause == 1").groupby("action").accName.unique()
@@ -115,8 +115,8 @@ def print_action_summary(eps_df, action):
 #     print_action_summary(eps_df, action)
 
 
-filePath2 = f"evaluation/stats/{runId}/statisticsWCBF.json"
-eps_df2 = load_repr1_to_eps(filePath2)
+file_path2 = f"evaluation/stats/{run_id}/statisticsWCBF.json"
+eps_df2 = load_repr1_to_eps(file_path2)
 
 
 def get_acc_violation_rate(eps_df, actions):
@@ -133,23 +133,69 @@ acc_violation_rates = {
 }
 
 
-x = np.arange(len(actions))  # the label locations
-width = 0.25  # the width of the bars
-multiplier = 0
+def plot_per_action(actions, acc_violation_rates, ylabel, title):
+    x = np.arange(len(actions))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
 
-fig, ax = plt.subplots(layout="constrained")
+    fig, ax = plt.subplots(layout="constrained")
 
-for attribute, measurement in acc_violation_rates.items():
-    offset = width * multiplier
-    rects = ax.bar(x + offset, measurement, width, label=attribute)
-    ax.bar_label(rects, padding=3)
-    multiplier += 1
+    for attribute, measurement in acc_violation_rates.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel("acc violation rate")
-ax.set_title("ACC violation rates")
-ax.set_xticks(x + width, actions)
-ax.legend(loc="upper left", ncols=3)
-# ax.set_ylim(0, 1)
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xticks(x + width, actions)
+    ax.legend(loc="upper left", ncols=3)
+    # ax.set_ylim(0, 1)
 
-plt.show()
+    plt.show()
+
+
+ylabel = "acc violation rate"
+title = "ACC violation rates"
+
+plot_per_action(actions, acc_violation_rates, ylabel, title)
+
+# eps_per_action = eps_df.groupby(["action", "compositeEpisodeNumber"]).count().terminationCause.reset_index("compositeEpisodeNumber").groupby("action").terminationCause.mean()
+
+
+def get_avg_eps_per_action(eps_df, actions):
+    eps_per_action_list = []
+    for action in actions:
+        action_df = eps_df.query("action == @action")
+        avg_eps = (
+            action_df.groupby("compositeEpisodeNumber").count().terminationCause.mean()
+        )
+        eps_per_action_list.append(avg_eps)
+    return eps_per_action_list
+
+
+eps_per_action = {
+    "WOCBF": get_avg_eps_per_action(eps_df, actions),
+    "WCBF": get_avg_eps_per_action(eps_df2, actions),
+}
+
+
+plot_per_action(actions, eps_per_action, "Episodes", "Episodes per action")
+
+
+def get_steps_per_action(eps_df, actions):
+    eps_per_action_list = []
+    for action in actions:
+        action_df = eps_df.query("action == @action")
+        avg_eps = action_df.groupby("compositeEpisodeNumber").localSteps.sum().mean()
+        eps_per_action_list.append(avg_eps)
+    return eps_per_action_list
+
+
+steps_per_action = {
+    "WOCBF": get_steps_per_action(eps_df, actions),
+    "WCBF": get_steps_per_action(eps_df2, actions),
+}
+
+plot_per_action(actions, steps_per_action, "Steps", "Steps per action")
