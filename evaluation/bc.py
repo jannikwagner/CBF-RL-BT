ca921c7c8342d153e71cd3675537cf57ebf9065f
Jannik Wagner <jannik.wagner@students.uni-mainz.de>
2023-07-07 18:34:48 +0200

    print tree

--- evaluation/bc.py
+++ evaluation/bc.py
@@ -32,25 +32,39 @@ A_MTB = Action("Move to bridge")
 actions = [A_MB2, A_MB1,A_MT2, A_MT1, A_MUP, A_MOB, A_MTB]
 
 @dataclass
-class PPA_Triple:
+class PPA:
     postcondition: Condition
     preconditions: Sequence[Condition]
     action: Action
 
 ppas = [
-    PPA_Triple(C_B2, (C_PB, ), A_MB2),
-    PPA_Triple(C_PB, (C_OB, ), A_MOB),
-    PPA_Triple(C_OB, (C_B1, C_T2, C_UP, ), A_MTB),
-    PPA_Triple(C_B1, (C_T1, C_UP, ), A_MB1),
-    PPA_Triple(C_T2, (C_B1, ), A_MT2),
-    PPA_Triple(C_T1, ( ), A_MT1),
-    PPA_Triple(C_UP, ( ), A_MUP),
+    PPA(C_B2, (C_PB, ), A_MB2),
+    PPA(C_PB, (C_OB, ), A_MOB),
+    PPA(C_OB, (C_B1, C_T2, C_UP, ), A_MTB),
+    PPA(C_B1, (C_T1, C_UP, ), A_MB1),
+    PPA(C_T2, (C_B1, ), A_MT2),
+    PPA(C_T1, ( ), A_MT1),
+    PPA(C_UP, ( ), A_MUP),
 ]
 
-def build_tree(goal: Condition, ppas: Sequence[PPA_Triple]):
+def build_tree(goal: Condition, ppas: Sequence[PPA]):
     ppa = [ppa for ppa in ppas if ppa.postcondition == goal][0]
     precondition_trees = [build_tree(precondition, ppas) for precondition in ppa.preconditions]
     return ("F", ppa.postcondition, ("S", *precondition_trees, ppa.action))
 
 
-print(build_tree(C_B2, ppas))
+def print_tree(tree: tuple, depth=0):
+    prefix = "| " * depth
+    if isinstance(tree, tuple):
+        symbol = tree[0]
+        print(prefix + symbol)
+        for child in tree[1:]:
+            print_tree(child, depth+1)
+    elif isinstance(tree, Action):
+        print(f"{prefix}[{tree.name}]")
+    elif isinstance(tree, Condition):
+        print(f"{prefix}({tree.name})")
+    else:
+        raise NotImplementedError
+
+print_tree(build_tree(C_B2, ppas))
